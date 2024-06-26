ce3ffc29902378e66575644c0951cb6678f0e127
BlackfireZZZ <Stipundrik@gmail.com>
2024-06-26 16:31:32 +0300

    Add and delete points in route

--- backend/app/models/location.py
+++ backend/app/models/location.py
@@ -6,4 +6,5 @@ class Location(db.Model):
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
     location_type = db.Column(db.String(255), nullable=False)
+    travel_id = db.Column(db.Integer, db.ForeignKey('travel.id'), nullable=False)
     order_num = db.Column(db.Integer, nullable=False)
--- backend/app/models/travel.py
+++ backend/app/models/travel.py
@@ -7,5 +7,22 @@ class Travel(db.Model):
                                   nullable=False)
     end_location_id = db.Column(db.Integer, db.ForeignKey('location.id'),
                                 nullable=False)
+    locations = db.relationship('Location', backref='travel', lazy=True)
+
+
+def delete_location(location_id):
+    location_to_delete = db.Location.query.filter_by(id=location_id).first()
+    deleted_order_num = location_to_delete.order_num
+    db.session.delete(location_to_delete)
+    for location in db.Location.query.filter_by(travel_id=location_to_delete.travel_id).all():
+        if location.order_num > deleted_order_num:
+            location.order_num -= 1
+    db.session.commit()
+
+
+def add_location(lat, long, loc_type, travel_id, order_num):
+    location = db.Location(latitude=lat, longitude=long, location_type=loc_type, travel_id=travel_id, order_num=order_num)
+    db.session.add(location)
+    db.session.commit()
 
 

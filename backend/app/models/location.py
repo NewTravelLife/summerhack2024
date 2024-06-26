468287a85b5ac689486b7643613905a0a6804ab1
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-06-26 14:34:34 +0400

    removed to_dict methods

--- backend/app/models/location.py
+++ backend/app/models/location.py
@@ -5,10 +5,3 @@ class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'latitude': self.latitude,
-            'longitude': self.longitude
-        }

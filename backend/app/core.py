8dd5ea9f38f9219d0aab0aa873e00c1b19474c86
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-06-26 22:52:25 +0400

    added Hotellook token to core

--- backend/app/core.py
+++ backend/app/core.py
@@ -3,3 +3,4 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
+app.config['HL_TOKEN'] = os.getenv('HL_TOKEN')

08b5fe3ba6fd80243481c70ecfe65997f763d7a2
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-06-27 00:44:10 +0400

    updated crud for location

--- backend/app/crud/location.py
+++ backend/app/crud/location.py
@@ -1,11 +1,20 @@
+from sqlalchemy import select
 
 from app.database import db
 from app.models.location import Location
+from app.models.travel import Travel
 
 
-def crud_create_location(latitude: float, longitude: float, location_type: str, order_number: int) -> Location:
-    location = Location(latitude=latitude, longitude=longitude, location_type=location_type, order_number=order_number)
+def crud_create_location(latitude: float, longitude: float, location_type: str, order_number: int, travel: Travel) -> Location:
+    location = Location(latitude=latitude, longitude=longitude, location_type=location_type, order_number=order_number,
+                        is_hotels_listed=False, hotels=[], travel=travel)
     db.session.add(location)
     db.session.commit()
     db.session.refresh(location)
+    return location
+
+
+def crud_get_location_by_id(id: int) -> Location | None:
+    stmt = select(Location).where(Location.id == id)
+    location = db.session.scalar(stmt)
     return location

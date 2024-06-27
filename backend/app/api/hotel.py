b5e2761ec4e60738dca4fc74a76567a3fb3330d4
BlackfireZZZ <Stipundrik@gmail.com>
2024-06-27 15:03:50 +0300

    New function

--- backend/app/api/hotel.py
+++ backend/app/api/hotel.py
@@ -1,11 +1,14 @@
 from typing import List
 
 from flask import Blueprint
-
+from flask import jsonify
 from app.crud.hotel import crud_get_hotels_by_location, crud_is_hotels_listed_by_location, crud_list_hotels_by_location, \
     crud_set_hotels_listed_by_location
-from app.crud.location import crud_get_location_by_id
+from app.crud.location import crud_get_location_by_id, crud_get_first_location_by_travel, \
+    crud_get_last_location_by_travel
+from app.crud.travel import crud_get_travel_by_id
 from app.schemas.hotel import HotelResponse
+from app.crud.google_down_maps import GoogleMaps
 
 api_hotel = Blueprint('hotel', __name__, url_prefix='/hotel')
 
@@ -24,3 +27,20 @@ def get_hotels_by_location_id(location_id):
     if len(hotels) < 1:
         return '', 404
     return {'hotels': hotels}, 200
+
+
+@api_hotel.route('/get_along_route/<travel_id>', methods=['GET'])
+def get_hotels_along_route(travel_id):
+    travel = crud_get_travel_by_id(travel_id)
+    start_location = crud_get_first_location_by_travel(travel)
+    end_location = crud_get_last_location_by_travel(travel)
+    if travel is None:
+        return '', 400
+    client = GoogleMaps()
+    route = client.get_direction(start_location, end_location)
+    hotels = client.get_places_along_route(route, 'lodging')
+    if len(hotels) < 1:
+        return '', 404
+    return {'hotels': hotels}, 200
+
+

5fc9e2c51cec31a1b2066c99884799464f857fc7
BlackfireZZZ <Stipundrik@gmail.com>
2024-06-28 02:07:53 +0300

    added endpoint to documents

--- frontend/src/components/TravelDocuments.jsx
+++ frontend/src/components/TravelDocuments.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../TravelDocuments.css';
 import downloadIcon from '../assets/download.png';
-import UploadButton from "./UploadButton.jsx";
-
-const travelDocuments = [
-    { name: 'Паспорт' },
-    { name: 'Билеты' },
-    { name: 'Виза' },
-    // Добавьте другие документы по мере необходимости
-];
+import UploadButton from './UploadButton.jsx';
 
 const TravelDocuments = () => {
+    const [travelDocuments, setTravelDocuments] = useState([]);
+
+    useEffect(() => {
+        // Функция для загрузки данных с сервера
+        const fetchTravelDocuments = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1/files/1'); // Замените 1 на travel_id, если требуется
+                if (!response.ok) {
+                    throw new Error('Ошибка загрузки данных');
+                }
+                const data = await response.json();
+                setTravelDocuments(data); // Установка полученных данных в состояние
+            } catch (error) {
+                console.error('Ошибка при загрузке документов:', error);
+            }
+        };
+
+        fetchTravelDocuments(); // Вызов функции загрузки данных
+    }, []); // Пустой массив зависимостей, чтобы выполнить useEffect только один раз
+
     return (
         <div>
             <div style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
@@ -27,7 +40,7 @@ const TravelDocuments = () => {
                                 <img src={downloadIcon} alt="Download" />
                             </button>
                             <span className="document-number">{index + 1}</span>
-                            <span className="document-name">{doc.name}</span>
+                            <span className="document-name">{doc}</span>
                         </li>
                     ))}
                 </ul>

13e6dee4f4c886302ba3a3a9af097a6320ce075f
Mikhail Fadin <fadin.edu@gmail.com>
2024-06-27 23:29:56 +0200

    Map

--- frontend/src/components/map.jsx
+++ frontend/src/components/map.jsx
@@ -3,11 +3,17 @@ import {GoogleMap, LoadScript, Polyline} from '@react-google-maps/api';
 const MapComponent = ({route}) => {
     const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
     console.log(route);
-    const pathCoordinates = route.map(loc => ({
-        lat: loc["location"]["lat"],
-        lng: loc["location"]["lng"]
-    }));
-    console.log(pathCoordinates)
+    // const pathCoordinates = route.map(loc => ({
+    //     lat: loc["location"]["lat"],
+    //     lng: loc["location"]["lng"]
+    // }));
+    const flightPlanCoordinates = [
+        {lat: 37.772, lng: -122.214},
+        {lat: 21.291, lng: -157.821},
+        {lat: -18.142, lng: 178.431},
+        {lat: -27.467, lng: 153.027},
+    ]
+    // console.log(pathCoordinates)
     const calculateCenter = (waypoints) => {
         const lats = waypoints.map(point => point.lat);
         const lngs = waypoints.map(point => point.lng);
@@ -15,25 +21,19 @@ const MapComponent = ({route}) => {
         const lngCenter = (Math.min(...lngs) + Math.max(...lngs)) / 2;
         return {lat: latCenter, lng: lngCenter};
     };
-    const center = calculateCenter(pathCoordinates);
+    const center = calculateCenter(flightPlanCoordinates);
     return (
         <LoadScript googleMapsApiKey={apiKey}>
             <GoogleMap
-                id="direction-example"
-                mapContainerStyle={{
-                    height: '800px',
-                    width: '1250px'
-                }}
-                zoom={7}
-                center={center}
+                initialZoom={3}
+                initialCenter={center}
             >
                 <Polyline
-                    path={pathCoordinates}
-                    options={{
-                        strokeColor: '#FF0000',
-                        strokeOpacity: 1.0,
-                        strokeWeight: 2,
-                    }}
+                    path={flightPlanCoordinates}
+                    strokeColor="#FF0000"
+                    strokeOpacity={1.0}
+                    strokeWeight={2}
+                    geodesic
                 />
             </GoogleMap>
         </LoadScript>

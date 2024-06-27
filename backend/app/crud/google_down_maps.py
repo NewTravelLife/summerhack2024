d8f1de26438546ffcc66935191ecc54ce207b99c
BlackfireZZZ <Stipundrik@gmail.com>
2024-06-27 13:57:52 +0300

    Placec along the route added

--- backend/app/crud/google_down_maps.py
+++ backend/app/crud/google_down_maps.py
@@ -1,4 +1,7 @@
 import googlemaps
+import math
+
+from flask import current_app
 
 
 def find_nearest(api_key, latitude, longitude, my_type, radius=1000):
@@ -20,12 +23,59 @@ def find_nearest(api_key, latitude, longitude, my_type, radius=1000):
     return places
 
 
-api_key = 'AIzaSyAaxy-BAdWOr9ERDtBsggTgNjFsLvYhlzQ'  # Replace with your actual API key
-latitude = 37.7749  # Example latitude
-longitude = -122.4194  # Example longitude
-my_type = ('restaurant', 'cafe')
-# types: ('restaurant', 'cafe'), 'museum', 'tourist_attraction', 'lodging'
+def get_direction(api_key, start_point, end_point):
+    gmaps = googlemaps.Client(key=api_key)
+    directions = gmaps.directions(start_point, end_point)
 
-nearest = find_nearest(api_key, latitude, longitude, my_type)
-for place in nearest:
+    # Получение координат маршрута
+    route_coords = []
+    for step in directions[0]['legs'][0]['steps']:
+        polyline = step['polyline']['points']
+        coords = googlemaps.convert.decode_polyline(polyline)
+        route_coords.extend(coords)
+    return route_coords
+
+
+def distance(lat1, lon1, lat2, lon2):
+    R = 6371.0  # радиус Земли в километрах
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+
+    dlon = lon2_rad - lon1_rad
+    dlat = lat2_rad - lat1_rad
+
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return R * c
+
+
+def get_places_along_route(api_key, route_coords, my_type, search_radius=1000, step_distance=10):
+    places = []
+    prev_coords = None
+    for coords in route_coords:
+        if prev_coords is not None:
+            dist = distance(prev_coords['lat'], prev_coords['lng'], coords['lat'], coords['lng'])
+            if dist >= step_distance:
+                places.extend(find_nearest(api_key, coords['lat'], coords['lng'], my_type, search_radius))
+                prev_coords = coords
+        else:
+            prev_coords = coords
+
+    # Final check for the last segment if no place found yet
+    if not places and route_coords:
+        places.extend(find_nearest(api_key, route_coords[-1]['lat'], route_coords[-1]['lng'], my_type, search_radius))
+
+    return places
+
+
+api_key = current_app.config['GOOGLE_MAPS_API_KEY']  # Replace with your actual API key
+start_location = (55.743926, 37.614491)  # Example start location
+end_location = (55.729832, 37.611938)  # Example end location
+my_type = 'restaurant'  # types: 'restaurant', 'cafe', 'museum', 'tourist_attraction', 'lodging'
+
+route_coords = get_direction(api_key, start_location, end_location)
+places = get_places_along_route(api_key, route_coords, my_type)
+for place in places:
     print(place)

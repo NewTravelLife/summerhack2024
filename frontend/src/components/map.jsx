677cb1a46e74d2c790712d7b96c642de8b12a2a7
Mikhail Fadin <fadin.edu@gmail.com>
2024-06-27 23:08:57 +0200

    Front

--- frontend/src/components/map.jsx
+++ frontend/src/components/map.jsx
@@ -1,94 +1,54 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import {
-    DirectionsRenderer,
-    DirectionsService,
-    GoogleMap,
-    LoadScript
-} from '@react-google-maps/api';
+import {GoogleMap, LoadScript} from '@react-google-maps/api';
+import {Marker, Polyline} from "react-leaflet";
 
-const MapComponent = ({}) => {
-    const [response, setResponse] = useState(null);
-    const [directionsServiceActive, setDirectionsServiceActive] = useState(true);
-    const [origin, setOrigin] = useState('');
-    const [destination, setDestination] = useState('');
-    const [inputOrigin, setInputOrigin] = useState('');
-    const [inputDestination, setInputDestination] = useState('');
-    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY; // Если не запускается - укажите ключ напрямую (не забудьте удалить при коммите)
-  
-    const directionsCallback = useCallback((res) => {
-      if (res !== null) {
-        if (res.status === 'OK') {
-          setResponse(res);
-          setDirectionsServiceActive(false);  // Stop the service after getting a response
-        } else {
-          console.log('response: ', res);
-        }
-      }
-    }, []);
-  
-    useEffect(() => {
-      if (origin && destination) {
-        setDirectionsServiceActive(true);
-      }
-    }, [origin, destination]);
-
-    const handleClick = () => {
-      setOrigin(inputOrigin);
-      setDestination(inputDestination);
+const MapComponent = ({route}) => {
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+    console.log(route);
+    const pathCoordinates = route.map(loc => ({
+        lat: loc["location"]["lat"],
+        lng: loc["location"]["lng"]
+    }));
+    console.log(pathCoordinates)
+    const calculateCenter = (waypoints) => {
+        const lats = waypoints.map(point => point.lat);
+        const lngs = waypoints.map(point => point.lng);
+        const latCenter = (Math.min(...lats) + Math.max(...lats)) / 2;
+        const lngCenter = (Math.min(...lngs) + Math.max(...lngs)) / 2;
+        return {lat: latCenter, lng: lngCenter};
     };
-
+    const center = calculateCenter(pathCoordinates);
     return (
-      <>
-      <div>
-      <input
-        type="text"
-        placeholder="Enter origin coordinates (lat,lng)"
-        value={inputOrigin}
-        onChange={(e) => setInputOrigin(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Enter destination coordinates (lat,lng)"
-        value={inputDestination}
-        onChange={(e) => setInputDestination(e.target.value)}
-      />
-      <button onClick={handleClick}>Get Directions</button>
-      </div>
-      <LoadScript googleMapsApiKey={apiKey}>
-        <GoogleMap
-          id="direction-example"
-          mapContainerStyle={{
-            height: '800px',
-            width: '1250px'
-          }}
-          zoom={7}
-          center={{
-            lat: 41.85073,
-            lng: -87.65126
-          }}
-        >
-          {directionsServiceActive && origin && destination && (
-            <DirectionsService
-              options={{
-                destination: destination,
-                origin: origin,
-                travelMode: 'DRIVING'
-              }}
-              callback={directionsCallback}
-            />
-          )}
-
-          {response && (
-            <DirectionsRenderer
-              options={{
-                directions: response
-              }}
-            />
-          )}
-        </GoogleMap>
-      </LoadScript>
-      </>
+        <LoadScript googleMapsApiKey={apiKey}>
+            <GoogleMap
+                id="direction-example"
+                mapContainerStyle={{
+                    height: '800px',
+                    width: '1250px'
+                }}
+                zoom={7}
+                center={center}
+            >
+                {route.map((location, index) => (
+                    <Marker
+                        key={index}
+                        position={{
+                            lat: location.location.lat,
+                            lng: location.location.lng
+                        }}
+                        title={location.name}
+                    />
+                ))}
+                <Polyline
+                    path={pathCoordinates}
+                    options={{
+                        strokeColor: '#FF0000',
+                        strokeOpacity: 1.0,
+                        strokeWeight: 2,
+                    }}
+                />
+            </GoogleMap>
+        </LoadScript>
     );
-  };
+};
 
 export default MapComponent;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
 import './index.css';
-import { Routes, Route } from 'react-router-dom';
 import MapComponent from "./components/map.jsx";
 import './App.css';
 import image1 from './assets/image1.jpg';
@@ -18,17 +17,16 @@ function setCords() {
 
 
 function App() {
-  const [showText, setShowText] = useState(false);
-  const [city, setCity] = useState('');
-  const [filteredAttractions, setFilteredAttractions] = useState([]);
-  const [temperature, setTemperature] = useState(null);
-  const [checked, setChecked] = useState({
-    word1: false,
-    word2: false,
-    word3: false,
-  });
-
-  const start = {
+    const [showText, setShowText] = useState(false);
+    const [city, setCity] = useState('');
+    const [filteredAttractions, setFilteredAttractions] = useState([]);
+    const [temperature, setTemperature] = useState(null);
+    const [checked, setChecked] = useState({
+        word1: false,
+        word2: false,
+        word3: false,
+    });
+    const start = {
         lat: 55.782982,
         lng: 37.63385
     }
@@ -37,146 +35,182 @@ function App() {
         lng: 30.362158
     }
 
-  
-  const handleSelect = (word) => {
-    setChecked((prevChecked) => ({...prevChecked, [word]:!prevChecked[word] }));
-  };
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowText(true);
-    }, 1000); // Задержка в 1 секунду перед показом текста
+    const handleSelect = (word) => {
+        setChecked((prevChecked) => ({
+            ...prevChecked,
+            [word]: !prevChecked[word]
+        }));
+    };
 
-    return () => clearTimeout(timer);
-  }, []);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowText(true);
+        }, 1000); // Задержка в 1 секунду перед показом текста
+
+        return () => clearTimeout(timer);
+    }, []);
 
 
     useEffect(() => {
         fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m')
-          .then((response) => response.json()) // Преобразуем ответ в JSON
-          .then((data) => {
-            if (data && data.current) {
-              setTemperature(data.current.temperature_2m); // Устанавливаем температуру в состояние
-            } else {
-                console.log("Ошибка загрузки температуры.");
+            .then((response) => response.json()) // Преобразуем ответ в JSON
+            .then((data) => {
+                if (data && data.current) {
+                    setTemperature(data.current.temperature_2m); // Устанавливаем температуру в состояние
+                } else {
+                    console.log("Ошибка загрузки температуры.");
+                }
+            })
+    }, []);
+
+    useEffect(() => {
+        filterAttractions(city);
+    }, [city]);
+
+    const attractions = [
+        {
+            image: image1,
+            title: 'Достопримечательность 1',
+            description: 'Описание достопримечательности 1.',
+            city: "Москва"
+        },
+        {
+            image: image1,
+            title: 'Достопримечательность 2',
+            description: 'Описание достопримечательности 2.',
+            city: "Санкт-Петербург"
+        },
+        {
+            image: image3,
+            title: 'Достопримечательность 3',
+            description: 'Описание достопримечательности 3.',
+            city: "Санкт-Петербург"
+        },
+        {
+            image: image3,
+            title: 'Достопримечательность 4',
+            description: 'Описание достопримечательности 4.',
+            city: "Москва"
+        },
+        {
+            image: image3,
+            title: 'Достопримечательность 5',
+            description: 'Очень крутая достопримечательность, тут короче такоооое, что вам точно нужно посетить её.',
+            city: "Москва"
+        },
+        {
+            image: image3,
+            title: 'Достопримечательность 6',
+            description: 'Описание достопримечательности 6.',
+            city: "Москва"
+        },
+        {
+            image: image3,
+            title: 'Достопримечательность 7',
+            description: 'Описание достопримечательности 7.',
+            city: "Москва"
+        },
+        {
+            image: image3,
+            title: 'Достопримечательность 8',
+            description: 'Описание достопримечательности 8.',
+            city: "Москва"
+        }
+    ];
+
+    const cities = ['Москва', 'Санкт-Петербург', 'Казань', 'Сочи'];
+
+    const filterAttractions = (selectedCity) => {
+        const filtered = attractions.map(attraction => {
+            if (selectedCity === '' || attraction.city === selectedCity) {
+                return attraction;
             }
-          })
-      }, []);
-  
-  useEffect(() => {
-    filterAttractions(city);
-  }, [city]);
+            return null;
+        }).filter(attraction => attraction !== null);
+        setFilteredAttractions(filtered);
+    };
 
-  const attractions = [
-    {
-      image: image1,
-      title: 'Достопримечательность 1',
-      description: 'Описание достопримечательности 1.',
-      city: "Москва"
-    },
-    {
-      image: image1,
-      title: 'Достопримечательность 2',
-      description: 'Описание достопримечательности 2.',
-      city: "Санкт-Петербург"
-    },
-    {
-      image: image3,
-      title: 'Достопримечательность 3',
-      description: 'Описание достопримечательности 3.',
-      city: "Санкт-Петербург"
-    },
-    {
-      image: image3,
-      title: 'Достопримечательность 4',
-      description: 'Описание достопримечательности 4.',
-      city: "Москва"
-    },
-    {
-      image: image3,
-      title: 'Достопримечательность 5',
-      description: 'Очень крутая достопримечательность, тут короче такоооое, что вам точно нужно посетить её.',
-      city: "Москва"
-    },
-    {
-      image: image3,
-      title: 'Достопримечательность 6',
-      description: 'Описание достопримечательности 6.',
-      city: "Москва"
-    },
-    {
-      image: image3,
-      title: 'Достопримечательность 7',
-      description: 'Описание достопримечательности 7.',
-      city: "Москва"
-    },
-    {
-      image: image3,
-      title: 'Достопримечательность 8',
-      description: 'Описание достопримечательности 8.',
-      city: "Москва"
-    }
-  ];
 
-  const cities = ['Москва', 'Санкт-Петербург', 'Казань', 'Сочи'];
-
-  const filterAttractions = (selectedCity) => {
-    const filtered = attractions.map(attraction => {
-      if (selectedCity === '' || attraction.city === selectedCity) {
-        return attraction;
-      }
-      return null;
-    }).filter(attraction => attraction!== null);
-    setFilteredAttractions(filtered);
-  };
-    
-
-  return (
-    <div>
-      <div className="header">
-        <div className="project-name">newtravel.life</div>
-        <div className="nav-buttons">
-          <button className="nav-button">Жильё</button>
-          <button className="nav-button">Питание</button>
-          <button className="nav-button">Мои путешествия</button>
-          <button className="nav-button">Поделиться</button>
-          <UploadButton uploadPath="api/travel/upload_file/1"/>
-          <Link to="/poisk">
-            <button className="nav-button">Создать путешествие</button>
-          </Link>
+    return (
+        <div>
+            <div className="header">
+                <div className="project-name">newtravel.life</div>
+                <div className="nav-buttons">
+                    <button className="nav-button">Жильё</button>
+                    <button className="nav-button">Питание</button>
+                    <button className="nav-button">Мои путешествия</button>
+                    <button className="nav-button">Поделиться</button>
+                    <UploadButton uploadPath="api/travel/upload_file/1"/>
+                    <Link to="/poisk">
+                        <button className="nav-button">Создать путешествие
+                        </button>
+                    </Link>
+                </div>
             </div>
+            <div className="text-container">
+                <div className="animated-text">
+                    С нами Ваше путешествие станет незабываемым
+                </div>
             </div>
-      <div className="text-container">
-        <div className="animated-text">
-          С нами Ваше путешествие станет незабываемым
-        </div>
-      </div>
-      <div className="input-container">
-        <select value={city} onChange={(e) => setCity(e.target.value)}>
-        {cities.map((cityOption, index) => (
-            <option key={index} value={cityOption}>{cityOption}</option>
-          ))}
-        </select>
-      </div>
-      <div className="attractions">
-        {filteredAttractions.map((attraction, index) => (
-          <div className="card" key={index}>
-            <img src={attraction.image} alt={attraction.title} className="card-image"/>
-            <h3 className="card-title">{attraction.title}</h3>
-            <p className="card-description">{attraction.description}</p>
-          </div>
-        ))}
-      </div>
-      <div>
-        <h1>Карта местности</h1>
-        <MapComponent start={start} end={end}/>
-      </div>
-      <div>
-            <h1>Температура сейчас:</h1>
-            {temperature !== null ? (<p>{temperature} °C</p>) :
-                (<p>Загрузка...</p>)}
-          </div>
-            
+            <div className="input-container">
+                <select value={city} onChange={(e) => setCity(e.target.value)}>
+                    {cities.map((cityOption, index) => (
+                        <option key={index}
+                                value={cityOption}>{cityOption}</option>
+                    ))}
+                </select>
+            </div>
+            <div className="attractions">
+                {filteredAttractions.map((attraction, index) => (
+                    <div className="card" key={index}>
+                        <img src={attraction.image} alt={attraction.title}
+                             className="card-image"/>
+                        <h3 className="card-title">{attraction.title}</h3>
+                        <p className="card-description">{attraction.description}</p>
+                    </div>
+                ))}
+            </div>
+            <div>
+                <h1>Карта местности</h1>
+                <MapComponent route={[[
+                    {
+                        "address": "University Embankment, 7, Sankt-Peterburg",
+                        location: {
+                            lat: 59.9404682,
+                            lng: 30.3005417
+                        },
+                        "name": "Zdaniye Dvenadtsati Kollegiy",
+                        "rating": 4.7,
+                        "user_ratings_total": 299
+                    },
+                    {
+                        "address": "University Embankment, 3, Sankt-Peterburg",
+                        location: {
+                            lat: 59.9415595,
+                            lng: 30.3047921
+                        },
+                        "name": "Kunstkamera",
+                        "rating": 4.4,
+                        "user_ratings_total": 22161
+                    },
+                    {
+                        "address": "Palace Square, 2, Sankt-Peterburg",
+                        location: {
+                            lat: 59.93983170000001,
+                            lng: 30.31455969999999
+                        },
+                        "name": "State Hermitage Museum",
+                        "rating": 4.8,
+                        "user_ratings_total": 58398
+                    }]]}/>
+            </div>
+            <div>
+                <h1>Температура сейчас:</h1>
+                {temperature !== null ? (<p>{temperature} °C</p>) :
+                    (<p>Загрузка...</p>)}
+            </div>
+
         </div>
     );
 

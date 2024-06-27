929bf846d973e1562bfa0ea4deb2eaa2c28ec40b
BlackfireZZZ <Stipundrik@gmail.com>
2024-06-27 20:06:02 +0300

    set locations order

--- backend/app/api/travel.py
+++ backend/app/api/travel.py
@@ -4,8 +4,10 @@ import uuid
 from flask import Blueprint, request, jsonify
 
 from app.core import app
+from app.crud.location import crud_get_location_by_id
 from app.crud.travel import crud_get_travel_by_id, crud_create_travel
 from app.crud.file import crud_create_file, crud_get_file_by_travel_id, crud_get_file_by_original_name_and_travel_id
+from app.database import db
 from app.services.disk import ProjectDisk
 
 api_travel = Blueprint('travel', __name__, url_prefix='/travel')
@@ -79,3 +81,18 @@ def download_file(travel_id, original_name):
         return '', 404
     project_disk.download_from_bucket(file.disk_name, os.path.join(app.config['UPLOAD_FOLDER'] + '/' + file.disk_name))
     return '', 200
+
+
+@api_travel.route('/set_locations/<travel_id>', methods=['DELETE'])
+def set_locations(travel_id):
+    if travel_id is None:
+        return '', 400
+    if not travel_id.isdigit():
+        return '', 400
+    data = request.get_json()
+    if data is None:
+        return '', 400
+    for set_location in data['locations']:
+        location = crud_get_location_by_id(set_location['id'])
+        location.order_number = set_location['order_number']
+    db.session.commit()

fdc588071d2e32a1fe5b358143dbf56eb8cb6fe6
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-06-27 19:24:59 +0400

    added file model

--- backend/app/models/travel.py
+++ backend/app/models/travel.py
@@ -7,6 +7,7 @@ from app.database import Base
 
 if TYPE_CHECKING:
     from .location import Location
+    from .file import File
 
 
 class Travel(Base):
@@ -14,4 +15,6 @@ class Travel(Base):
 
     id: Mapped[int] = mapped_column(Integer(), primary_key=True)
     locations: Mapped[List['Location']] = relationship(back_populates='travel',
+                                                       lazy='selectin')
+    files: Mapped[List['File']] = relationship(back_populates='travel',
                                                        lazy='selectin')

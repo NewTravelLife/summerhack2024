e952cd332fbe04d35216a7c685cf1a00b49bb830
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-06-27 21:34:41 +0400

    added new endpoints

--- backend/app/api/travel.py
+++ backend/app/api/travel.py
@@ -2,16 +2,30 @@ from typing import cast
 
 from flask import Blueprint, request, jsonify, current_app
 
-from app.crud.location import crud_create_location
+from app.crud.location import crud_create_location, crud_get_location_by_id
 from app.crud.travel import crud_get_travel_by_id, crud_create_travel
-from app.schemas.travel import TravelCreateRequest, TravelCreateResponse
+from app.schemas.travel import TravelCreateRequest, TravelCreateResponse, TravelGetLocationsResponse, TravelLocation
 from app.services.gm.googlemaps import GoogleMaps
 
 api_travel = Blueprint('travel', __name__, url_prefix='/travel')
 
 
-@api_travel.route('/test/<travel_id>', methods=['GET'])
-def get_along_route(travel_id: int):
+@api_travel.route('/locations/<travel_id>', methods=['GET'])
+def get_locations(travel_id: int):
+    if travel_id is None:
+        return '', 400
+    travel = crud_get_travel_by_id(travel_id)
+    if travel is None:
+        return '', 404
+    response: TravelGetLocationsResponse = TravelGetLocationsResponse(locations=[])
+    for location in travel.locations:
+        response['locations'].append(TravelLocation(id=int(location.id), latitude=location.latitude,
+            longitude=location.longitude, location_type=location.location_type, order_number=location.order_number))
+    return jsonify(response), 200
+
+
+@api_travel.route('/route/<travel_id>', methods=['GET'])
+def get_route_with_places(travel_id: int):
     travel = crud_get_travel_by_id(travel_id)
     if travel is None:
         return '', 400
@@ -23,27 +37,15 @@ def get_along_route(travel_id: int):
     return {'items': items, 'route': route}, 200
 
 
-@api_travel.route('/get/<travel_id>', methods=['GET'])
-def get_travel(travel_id):
-    if travel_id is None:
-        return '', 400
-    if not travel_id.isdigit():
-        return '', 400
-    travel = crud_get_travel_by_id(travel_id)
-    if travel is None:
-        return '', 404
-    return travel.id, 200
-
-
-@api_travel.route('/create', methods=['POST'])
+@api_travel.route('/', methods=['POST'])
 def create_travel():
     data = request.get_json()
-    if data is None or not isinstance(data, TravelCreateRequest):
+    if data is None:
         return '', 400
     data = cast(TravelCreateRequest, data)
     travel = crud_create_travel([])
-    crud_create_location(data['start_lat'], data['start_lon'], 'start', 1, travel)
-    crud_create_location(data['end_lat'], data['end_lon'], 'end', 2, travel)
-    response: TravelCreateResponse = {'travel_id': int(travel.id)}
+    crud_create_location(data['start_lat'], data['start_lon'], 'start', 0, travel)
+    crud_create_location(data['end_lat'], data['end_lon'], 'end', 1, travel)
+    response: TravelCreateResponse = TravelCreateResponse(travel_id=int(travel.id))
     return jsonify(response), 200
 

7d5e3cb660e93e7b4903f270d2be50f7172a6211
luckMillooney <165415079+luckMillooney@users.noreply.github.com>
2024-06-28 02:04:38 +0200

    Update poisk.jsx

--- frontend/src/components/poisk.jsx
+++ frontend/src/components/poisk.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, {useRef, useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
-import React, {useRef, useState} from 'react';
-import {Link} from 'react-router-dom';
 import {Autocomplete, LoadScript} from '@react-google-maps/api';
 import '../App.css';
 import '../index.css';
@@ -17,7 +15,7 @@ import getCords from "./travelManager";
 import {useCookies} from "react-cookie";
 
 
-const Poisk = () => {
+const Poisk =()=>{
     const [inputOrigin, setInputOrigin] = useState('');
     const [inputDestination, setInputDestination] = useState('');
     const [origin, setOrigin] = useState('');
@@ -39,29 +37,7 @@ const Poisk = () => {
       };
     
 
-      const attractions = [
-        {
-          image: image1,
-          title: 'Достопримечательность 1'
-        },
-        {
-          image: image2,
-          title: 'Достопримечательность 2'
 
-        },
-    
-        {
-          image: image3,
-          title: 'Достопримечательность 4'
-
-
-        },
-        {
-          image: image4,
-          title: 'Достопримечательность 8'
-
-        }
-      ];
       const start = {
         lat: 55.782982,
         lng: 37.63385
@@ -81,14 +57,9 @@ const Poisk = () => {
           }
         })
     }, []);
-    };
 
-    const handleSelect = (word) => {
-        setChecked((prevChecked) => ({
-            ...prevChecked,
-            [word]: !prevChecked[word]
-        }));
-    };
+
+
 
     const handlePlaceChanged = (type) => {
         const autocomplete = type === 'origin' ? originAutocompleteRef.current : destinationAutocompleteRef.current;
@@ -154,111 +125,20 @@ const Poisk = () => {
       
       <div className="text-container0">
                   <div className="animated-text0">
-                          Помошник в составлении вашего идеального путешествия
+                          Помощник в составлении вашего идеального путешествия
                   </div>
               </div>
               <img src={bac} className='bac' alt="" />
               <div className='box-container'>
                   <h1 className='textt'>Выбери свой маршрут</h1>
-                  <p className='sposob'>Способ передвижения:</p>
-                  <div className="checkpoints">
-        <label>
-          Авто
-          <input type="checkbox" className='chek0' checked={checked.word1} onChange={() => handleSelect('word1')} />
-        </label>
-        <br />
-        <label>
-          ЖД
-          <input type="checkbox" className='chek1' checked={checked.word2} onChange={() => handleSelect('word2')} />
-        </label>
-        <br />
-        <label>
-          АВИА
-          <input type="checkbox" className='chek2' checked={checked.word3} onChange={() => handleSelect('word3')} />
-        </label>
-      </div>
+
       <div className='forma'>
-          <button className='poisk'>Составить</button>
-      <img src={strelkaUrl} className='strelka' alt="" />
-          <div className='Otpravka' >
-              <p>Город отправления:</p>
-              <input className='input-otprav' placeholder="Название" type="text" />
-          </div>
-          <div className='Priezd'  >
-              <p>Город прибытия:</p>
-              <input className='input-otprav ' placeholder="Название" type="text" />
-          </div>
-      </div>
-      </div>
-      <div className='placeholder'>
-      </div>
-      <div className='Upload-button'>
-      <UploadButton uploadPath="api/travel/upload_file/1"/>
-      </div>
-      <div className="WeatherTemperature"> 
-            <p>Температура на маршруте:</p>
-            {temperature !== null ? (<h1>{temperature} °C</h1>) :
-                (<p>Загрузка...</p>)}
-          </div>
-      
-    </div>
-
-  );
-    return (
-        <div>
-            <div className="header">
-                <Link to="/">
-                    <div className="project-name">newtravel.life</div>
-                </Link>
-                <div className="nav-buttons">
-                    <button className="nav-button">Жильё</button>
-                    <button className="nav-button">Питание</button>
-                    <button className="nav-button">Мои путешествия</button>
-                    <Link to="/poisk">
-                        <button className="nav-button">Создать путешествие
-                        </button>
-                    </Link>
-                </div>
-            </div>
-
-            <div className="text-container0">
-                <div className="animated-text0">
-                    Помошник в составлении вашего идеального путешествия
-                </div>
-            </div>
-            <img src={bac} className='bac' alt=""/>
-            <div className='box-container'>
-                <h1 className='textt'>Выбери свой маршрут</h1>
-                <p className='sposob'>Способ передвижения:</p>
-                <div className="checkpoints">
-                    <label>
-                        Авто
-                        <input type="checkbox" className='chek0'
-                               checked={checked.word1}
-                               onChange={() => handleSelect('word1')}/>
-                    </label>
-                    <br/>
-                    <label>
-                        ЖД
-                        <input type="checkbox" className='chek1'
-                               checked={checked.word2}
-                               onChange={() => handleSelect('word2')}/>
-                    </label>
-                    <br/>
-                    <label>
-                        АВИА
-                        <input type="checkbox" className='chek2'
-                               checked={checked.word3}
-                               onChange={() => handleSelect('word3')}/>
-                    </label>
-                </div>
-                <div className='forma'>
                     <button className='poisk'
                             onClick={handleComposeRoute}>Составить
                     </button>
                     <img src={strelkaUrl} className='strelka' alt=""/>
                     <div className='Otpravka'>
-                        <p>Место отправления:</p>
+                        <p >Место отправления:</p>
                         <LoadScript googleMapsApiKey={apiKey}
                                     libraries={['places']}>
                             <Autocomplete
@@ -294,19 +174,24 @@ const Poisk = () => {
                         </LoadScript>
                     </div>
                 </div>
-            </div>
-            <div className="animated-text1">
-                Готовые решения:
-            </div>
-            <div className='box2'>
-                <p className='popnaprav'>Популярные направления:</p>
-                {attractions.map((attraction, index) => (
-                    <img key={index} src={attraction.image}
-                         alt={attraction.title}/>
-                ))}
-            </div>
+      </div>
+      <div className='placeholder'>
+      </div>
+      <div className='Upload-button'>
+      <UploadButton uploadPath="api/travel/upload_file/1"/>
+      </div>
+      <div className="WeatherTemperature"> 
+            <p>Температура на маршруте:</p>
+            {temperature !== null ? (<h1>{temperature} °C</h1>) :
+                (<p>Загрузка...</p>)}
         </div>
-    );
+
+
+    
+    </div>
+
+  );
+}
 
 
 export default Poisk;
